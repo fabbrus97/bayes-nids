4cbc4ef1ae638d9861ee97a5b84d5827e4f11da0
Simone Fabbri <fabbrus@gmail.com>
2023-09-12 21:43:27 +0200

    first commit

---
+++ udp_tools/ntp-4.2.6p5/ntpdc/version.c
@@ -0,0 +1,5 @@
+/*
+ * version file for ntpdc
+ */
+#include <config.h>
+const char * Version = "ntpdc 4.2.6p5@1.2349 Wed Aug 30 18:37:14 UTC 2023 (2)";
---
+++ udp_tools/ntp-4.2.6p5/ntpq/version.c
@@ -0,0 +1,5 @@
+/*
+ * version file for ntpq
+ */
+#include <config.h>
+const char * Version = "ntpq 4.2.6p5@1.2349 Wed Aug 30 18:37:16 UTC 2023 (2)";
