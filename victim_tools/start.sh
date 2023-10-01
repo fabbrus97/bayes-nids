94aa00e390af7012d6d5124449d0b7cf8510a006
Simone Fabbri <fabbrus@gmail.com>
2023-10-01 11:15:44 +0200

    added az cli yaml

--- victim_tools/start.sh
+++ victim_tools/start.sh
@@ -4,4 +4,12 @@
 telnetd #-l /bin/ash
 
 mkdir -p /var/log/lighttpd/
+mkdir -p /opt/lighttpd
+mkdir -p /var/www
+mkdir -p /var/ssl
+
+cp -r /lighttpd/config/lighttpd.conf /opt/lighttpd
+cp -r /lighttpd/www/* /var/www/
+cp -r /lighttpd/ssl/* /var/ssl/
+
 lighttpd -D -f /opt/lighttpd/lighttpd.conf
