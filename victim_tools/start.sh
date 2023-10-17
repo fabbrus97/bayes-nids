c687e017483206e5958c722308e54a98ac9947c5
simone <fabbrus@gmail.com>
2023-10-17 16:07:08 +0200

    victim files fixed

--- victim_tools/start.sh
+++ victim_tools/start.sh
@@ -1,21 +1,20 @@
 #!/bin/ash
 
-
-telnetd #-l /bin/ash
-
 if [ $# -gt 0 ] ; then
     if [[ $1 == nohttp ]] ; then
-        exit
+	telnetd -F
     fi  
+else
+    telnetd #-l /bin/ash
+    
+    mkdir -p /var/log/lighttpd/
+    mkdir -p /opt/lighttpd
+    mkdir -p /var/www
+    mkdir -p /var/ssl
+    
+    cp -r /lighttpd/config/lighttpd.conf /opt/lighttpd
+    cp -r /lighttpd/www/* /var/www/
+    cp -r /lighttpd/ssl/* /var/ssl/
+    
+    lighttpd -D -f /opt/lighttpd/lighttpd.conf
 fi
-
-mkdir -p /var/log/lighttpd/
-mkdir -p /opt/lighttpd
-mkdir -p /var/www
-mkdir -p /var/ssl
-
-cp -r /lighttpd/config/lighttpd.conf /opt/lighttpd
-cp -r /lighttpd/www/* /var/www/
-cp -r /lighttpd/ssl/* /var/ssl/
-
-lighttpd -D -f /opt/lighttpd/lighttpd.conf
