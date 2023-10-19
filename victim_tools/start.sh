f9d09acf890d9744d7401bede7addde364647ec6
simone <fabbrus@gmail.com>
2023-10-19 17:34:07 +0200

    removed useless parts

--- victim_tools/start.sh
+++ victim_tools/start.sh
@@ -8,13 +8,13 @@ else
     telnetd #-l /bin/ash
     
     mkdir -p /var/log/lighttpd/
-    mkdir -p /opt/lighttpd
-    mkdir -p /var/www
-    mkdir -p /var/ssl
+    #mkdir -p /opt/lighttpd
+    #mkdir -p /var/www
+    #mkdir -p /var/ssl
     
-    cp -r /lighttpd/config/lighttpd.conf /opt/lighttpd
-    cp -r /lighttpd/www/* /var/www/
-    cp -r /lighttpd/ssl/* /var/ssl/
+    #cp -r /lighttpd/config/lighttpd.conf /opt/lighttpd
+    #cp -r /lighttpd/www/* /var/www/
+    #cp -r /lighttpd/ssl/* /var/ssl/
     
     lighttpd -D -f /opt/lighttpd/lighttpd.conf
 fi
